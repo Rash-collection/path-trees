f27c1e2c8bc37f223f82dfc5ee6b5e2921795feb
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-03-26 22:57:42 +0200

    the main class for test-runs on dev-phase.. initial commit

---
+++ src/main/java/pathz/RunTest.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pathz;
+
+/**
+ * <p>Main class for developing tests, or for user's testings.</p>
+ * @author rash4
+ */
+public class RunTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
