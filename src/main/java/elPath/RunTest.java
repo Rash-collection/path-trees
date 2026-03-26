446020bd6662f7e3c0d5715f4e2716cb60885aad
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-03-26 23:28:42 +0200

    changed main package name from pathz -to-> elPath. PS: it contains the TestRun.java the main class for tests.

--- src/main/java/elPath/RunTest.java
+++ src/main/java/elPath/RunTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package pathz;
+package elPath;
 
 /**
  * <p>Main class for developing tests, or for user's testings.</p>
